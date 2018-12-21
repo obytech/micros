969667ec8c3e05a8e9d2cc7e2104abc1fe33c915
obytech <obytech@outlook.com>
2018-12-21 16:48:50 +0200

    Ribbon support

--- services/src/main/kotlin/com/obytech/micros/config/PingConfig.kt
+++ services/src/main/kotlin/com/obytech/micros/config/PingConfig.kt
@@ -1,10 +1,12 @@
 package com.obytech.micros.config
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @Configuration
 @Profile("ping")
+@RibbonClient("pong-service")
 @EnableScheduling
 class PingConfig
